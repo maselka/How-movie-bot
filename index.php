828b0c2c82bcb7e04e2c52f3246195590d67c405
maselka <gabdullinmr@gmail.com>
2019-06-28 16:45:12 +0300

    Добавил вроде бы рабочий код

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include('vendor/autoload.php');
+use Telegram\Bot\Api;
+
+$telegram = new Api('854559704:AAFfCPdSB-SfwwX-QNWIplVUmeV8cd-VjHk');
+$result = $telegram -> getWebhookUpdates();
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+$name = $result["message"]["from"]["username"];
+
+if($text) {
+    if ($text == "/start") {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' =>'Добро пожаловать!' ]);
+    } elseif($text == "/sayHello" and $name) {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => '\'Hello, ' . $name . '!']);
+    } elseif ($text == "/sayHello" and !$name) {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'Hello, незнакомец!' ]);
+    } else {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'Запрос не является командой' ]);
+    }
+}
+?>
