3a3bc9299056c99fd620e67dbe3a79dc69e701a6
maselka <gabdullinmr@gmail.com>
2019-07-01 16:24:08 +0300

    123

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ if ($text) {
     if ($text == "/start") {
           $reply = "Добро пожаловать в бота!";
           $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
-          $teleram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+          $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
     } elseif ($text == "Привет бот") {
         if ($name) {
             $reply = 'Hello, ' . $name . '!';
         } else {
             $reply = 'Hello, незнакомец!';
         }
-        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
     }
 }
 ?>
