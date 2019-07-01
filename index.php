3ebc8cc8c8a00ec3c716768b14c18b540736f5b2
maselka <gabdullinmr@gmail.com>
2019-07-01 21:27:35 +0300

    добавление api

--- index.php
+++ index.php
@@ -9,16 +9,12 @@ $text = $result["message"]["text"];
 $chat_id = $result["message"]["chat"]["id"];
 $name = $result["message"]["from"]["username"];
 $keyboard = [['Какой фильм посмотреть?'],['Расскажи мне о фильме..']];
-//$keyboardGenres = []
 
-//$genge = new HttpRequest();
-//$genge->setUrl('https://api.themoviedb.org/3/genre/movie/list');
-//$genge->setMethod(HTTP_METH_GET);
-//
-//$genge->setQueryData(array(
-//    'language' => 'ru-RU',
-//    'api_key' => '951aefe4839143b19cb846c5002fb7a9'
-//));
+
+$token = new Tmdb\ApiToken('951aefe4839143b19cb846c5002fb7a9');
+$client = new Tmdb\Client ($token);
+$body = new http\Message\Body;
+$body->append('{}');
 
 
 if($text) {
@@ -32,11 +28,28 @@ if($text) {
         } else {
             $reply = 'Hello, незнакомец!';
         }
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
     } elseif($text == "Какой фильм посмотреть?") {
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-        //$reply = $genge;
-        $telegram ->sendMessage([ 'chat_id' => $chat_id, 'text' => 'никакой', 'reply_markup' => $reply_markup ]);
+        $reply = 'Какой жанр?';
+        $telegram ->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        //$keyboardGenres =
+        //$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
     }  //elseif($text == "Расскажи мне о фильме..") {}
 }
+
+$body = new http\Message\Body;
+$body->append('{}');
+
+$request->setRequestUrl('https://api.themoviedb.org/3/genre/movie/list');
+$request->setRequestMethod('GET');
+$request->setBody($body);
+
+$request->setQuery(new http\QueryString(array(
+    'language' => 'ru-RU',
+    'api_key' => '951aefe4839143b19cb846c5002fb7a9'
+)));
+
+$client->enqueue($request)->send();
+$response = $client->getResponse();
+
+echo $response->getBody();
