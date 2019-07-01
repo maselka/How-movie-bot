12f5c4f26ad1a03b1f298008be78f1afc7fb0cac
maselka <gabdullinmr@gmail.com>
2019-07-01 19:41:25 +0300

    123

--- index.php
+++ index.php
@@ -1,35 +1,16 @@
 <?php
 
-$token = "854559704:AAFfCPdSB-SfwwX-QNWIplVUmeV8cd-VjHk";
-$bot = new TelegramBot\Api\Client($token);
-// команда для start
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
+include('vendor/autoload.php');
+use Telegram\Bot\Api;
 
-// команда для помощи
-$bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - вывод справки';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
+$telegram = new Api('854559704:AAFfCPdSB-SfwwX-QNWIplVUmeV8cd-VjHk');
+$result = $telegram -> getWebhookUpdates();
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+$name = $result["message"]["from"]["username"];
+$keyboard = [['Какой фильм посмотреть?']['Расскажи мне о фильме..']];
+//$keyboardGenres = []
 
-$bot->run();
-
-?>
-
-//include('vendor/autoload.php');
-//use Telegram\Bot\Api;
-//
-//$telegram = new Api('854559704:AAFfCPdSB-SfwwX-QNWIplVUmeV8cd-VjHk');
-//$result = $telegram -> getWebhookUpdates();
-//$text = $result["message"]["text"];
-//$chat_id = $result["message"]["chat"]["id"];
-//$name = $result["message"]["from"]["username"];
-//$keyboard = [['Какой фильм посмотреть?']['Расскажи мне о фильме..']];
-////$keyboardGenres = []
-//
 //$genge = new HttpRequest();
 //$genge->setUrl('https://api.themoviedb.org/3/genre/movie/list');
 //$genge->setMethod(HTTP_METH_GET);
@@ -38,24 +19,24 @@ $bot->run();
 //    'language' => 'ru-RU',
 //    'api_key' => '951aefe4839143b19cb846c5002fb7a9'
 //));
-//
-//
-//if($text) {
-//    if ($text == "/start") {
-//        $reply = "Привет, я расскажу тебе все об интересном тебе фильме!";
-//        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-//        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-//    } elseif($text == "Привет бот") {
-//        if ($name) {
-//            $reply = 'Hello, ' . $name . '!';
-//        } else {
-//            $reply = 'Hello, незнакомец!';
-//        }
-//        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-//        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-//    } elseif($text == "Какой фильм посмотреть?") {
-//        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-//        $reply = $genge;
-//        $telegram ->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-//    }
-//}
+
+
+if($text) {
+    if ($text == "/start") {
+        $reply = "Привет, я расскажу тебе все об интересном тебе фильме!";
+        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+    } elseif($text == "Привет бот") {
+        if ($name) {
+            $reply = 'Hello, ' . $name . '!';
+        } else {
+            $reply = 'Hello, незнакомец!';
+        }
+        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+    } elseif($text == "Какой фильм посмотреть?") {
+        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+        $reply = $genge;
+        $telegram ->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+    }  //elseif($text == "Расскажи мне о фильме..") {}
+}
