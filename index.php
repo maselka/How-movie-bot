4a20d767348e3505b7a098153c760272f06cf393
maselka <gabdullinmr@gmail.com>
2019-07-01 21:36:41 +0300

    123

--- index.php
+++ index.php
@@ -32,24 +32,10 @@ if($text) {
     } elseif($text == "Какой фильм посмотреть?") {
         $reply = 'Какой жанр?';
         $telegram ->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        $genres = $client -> getGenresApi () -> getGenres ();
+        $reply = $genres;
+        $telegram ->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
         //$keyboardGenres =
         //$reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
     }  //elseif($text == "Расскажи мне о фильме..") {}
 }
-
-$body = new http\Message\Body;
-$body->append('{}');
-
-$request->setRequestUrl('https://api.themoviedb.org/3/genre/movie/list');
-$request->setRequestMethod('GET');
-$request->setBody($body);
-
-$request->setQuery(new http\QueryString(array(
-    'language' => 'ru-RU',
-    'api_key' => '951aefe4839143b19cb846c5002fb7a9'
-)));
-
-$client->enqueue($request)->send();
-$response = $client->getResponse();
-
-echo $response->getBody();
