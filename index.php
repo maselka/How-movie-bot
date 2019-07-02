1a5d17aa14d5e0fca44a0b3ce73365d6483fc91c
maselka <gabdullinmr@gmail.com>
2019-07-02 19:05:23 +0300

    отладка

--- index.php
+++ index.php
@@ -18,7 +18,8 @@ if($text) {
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
     } elseif ($text) {
         $result = $client->getSearchApi()->searchMovies($text);
-        $resultArray = json_decode($result ) -> results;
+        $resultObject = json_decode($result);
+        $resultArray = $resultObject -> results;
 
         foreach ($resultArray as $value) {
         $posterUrl = $value -> poster_path;
