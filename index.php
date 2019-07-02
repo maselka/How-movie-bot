6294510f1e2b58c517b2b1cc6a91e084e128cdaa
maselka <gabdullinmr@gmail.com>
2019-07-02 19:01:34 +0300

    дабавил отправку фильма

--- index.php
+++ index.php
@@ -8,33 +8,22 @@ $result = $telegram -> getWebhookUpdates();
 $text = $result["message"]["text"];
 $chat_id = $result["message"]["chat"]["id"];
 $name = $result["message"]["from"]["username"];
-$keyboard = [['Какой фильм посмотреть?'],['Расскажи мне о фильме..']];
 
 $token = new Tmdb\ApiToken('951aefe4839143b19cb846c5002fb7a9');
 $client = new Tmdb\Client ($token);
-$keyboardGenres = array();
-
-
-$tempArray = array();
-$genres = $client -> getGenresApi () -> getGenres ();
-$GenresArray = $genres -> genres;
-foreach ($GenresArray as &$value) {
-    $temArray = $value->name;
-    $keyboardGenres[] = $tempArray;
-}
-
 
 if($text) {
     if ($text == "/start") {
-        $reply = "Привет, я расскажу тебе все об интересном тебе фильме!";
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => true ]);
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    } elseif($text == "Какой фильм посмотреть?") {
-        $reply = 'Какой жанр?';
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboardGenres, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+        $reply = "Привет, если ты напишешь название фильма, то я расскажу тебе о нем все что знаю";
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
+    } elseif ($text) {
+        $result = $client->getSearchApi()->searchMovies($text);
+        $resultArray = json_decode($result ) -> results;
 
-
-
+        foreach ($resultArray as $value) {
+        $posterUrl = $value -> poster_path;
+        $movieInfo = $value -> overview;
+        $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $posterUrl, 'caption' => $movieInfo ]);
+        }
     }
 }
