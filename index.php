d74ebbb0dd525bae7cba86e30754b48724a80a93
maselka <gabdullinmr@gmail.com>
2019-07-04 00:07:40 +0300

    отладка

--- index.php
+++ index.php
@@ -40,11 +40,11 @@ function caсhResponse ($db, $response, $request) {
     return $id;
 }
 
-function getResponse($db, $request) {
-    $db->where('request', $request);
-    $response  =  $db -> getValue('cach_requests', 'response');
-    return  json_decode($response);
-}
+//function getResponse($db, $request) {
+//    $db->where('request', $request);
+//    $response  =  $db -> getValue('cach_requests', 'response');
+//    return  json_decode($response);
+//}
 
 if($text) {
     if ($text == "/start") {
