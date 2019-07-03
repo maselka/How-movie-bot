ff74921c0faf05bdc5859175686b086ec620decc
maselka <gabdullinmr@gmail.com>
2019-07-03 20:19:39 +0300

    отладка

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 include('vendor/autoload.php');
 use Telegram\Bot\Api;
 
-$db  =  new  MysqliDb ( 'localhost' , 'root' , '123456789' , ' databaseName ' );
+$db  =  new  MysqliDb ( 'localhost' , 'b1597e3a08d730' , '123456789' , 'heroku_34fcf0748940255' );
 const DB_HOST = 'eu-cdbr-west-02.cleardb.net';
-const DB_USER = 'root';
+const DB_USER = 'b1597e3a08d730';
 const DB_PASS = '9a13c73f';
-const DB_NAME = 'b1597e3a08d730';
-
 
 $telegram = new Api('854559704:AAFfCPdSB-SfwwX-QNWIplVUmeV8cd-VjHk');
 $result = $telegram -> getWebhookUpdates();
@@ -28,27 +26,35 @@ function getTextUnderPoster($arrayWithInfo) {
     ' . $arrayWithInfo['overview'];
 }
 
-function insertMoviesInDb ($searh, $arrayWithMovies) {
-    $id = $db->insert('searhQueries', $searh);
-    foreach ($arrayWithMovies as $value)
-    $id = $db->insert('movies', $value);
+function caсhResponse ($db, $response, $request) {
+    $row = [
+      'response' =>  json_encode($response),
+      'request' => $request,
+      'date' => new DateTime()
+    ];
+    $id = $db->insert('cach_response', $row);
     return $id;
 }
 
-function GetMovieOfDb($searh) {
-
+function getResponse($db, $request) {
+    $db->where('request', $request);
+    $response  =  $db -> getValue ('cach_requests', "response");
+   return  json_decode($response);
 }
 
 if($text) {
     if ($text == "/start") {
-        $reply = "Привет, если ты напишешь название фильма, то я расскажу тебе о нем все что знаю";
+        $reply = "Привет, если ты напишешь какую нибудь фразу или слово, то я покажу тебе 3 фильма связанных с этим выражением";
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
     } elseif ($text) {
-        $db->get("query", 1);
-        $user = $db->getOne ("users");
-        $result = $client->getSearchApi()->searchMovies($text);
+        $result = getResponse($text);
+        if (!$result) {
+            $result = $client->getSearchApi()->searchMovies($text);
+            caсhResponse ($result, $text);
+        }
         for($i=0; $i<3; $i++) {
             $telegram->sendPhoto(['chat_id' => $chat_id, 'parse_mode' => 'HTML', 'photo' => getUrlPoster($result['results'][$i]), 'caption' => getTextUnderPoster($result['results'][$i])]);
         }
+
     }
 }
