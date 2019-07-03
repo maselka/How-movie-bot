821fd35ce73da3dfdbbeb0a0fbad8b3836cb5156
maselka <gabdullinmr@gmail.com>
2019-07-03 20:25:06 +0300

    отладка

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include('vendor/autoload.php');
 use Telegram\Bot\Api;
 
-$db  =  new  MysqliDb ( 'localhost' , 'b1597e3a08d730' , '123456789' , 'heroku_34fcf0748940255' );
+$db  =  new  MysqliDb ( 'eu-cdbr-west-02.cleardb.net' , 'b1597e3a08d730' , '123456789' , 'heroku_34fcf0748940255' );
 const DB_HOST = 'eu-cdbr-west-02.cleardb.net';
 const DB_USER = 'b1597e3a08d730';
 const DB_PASS = '9a13c73f';
@@ -38,8 +38,8 @@ function caсhResponse ($db, $response, $request) {
 
 function getResponse($db, $request) {
     $db->where('request', $request);
-    $response  =  $db -> getValue ('cach_requests', "response");
-   return  json_decode($response);
+    $response  =  $db -> getValue('cach_requests', 'response');
+    return  json_decode($response);
 }
 
 if($text) {
