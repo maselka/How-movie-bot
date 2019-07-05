f3ba7dfe61766486a825afd1037900f683115d3a
maselka <gabdullinmr@gmail.com>
2019-07-06 01:42:11 +0300

    отладка

--- index.php
+++ index.php
@@ -26,10 +26,10 @@ if($text) {
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
     } elseif ($text) {
         //$result = getResponse($db, $text);
-        \\if (!$result) {
-        \\    $result = $client->getSearchApi()->searchMovies($text);
-        \\    insertRow($db, $id, $text, $result);
-        \\}
+        //if (!$result) {
+        //    $result = $client->getSearchApi()->searchMovies($text);
+        //    insertRow($db, $id, $text, $result);
+        //}
         for($i=0; $i<3; $i++) {
             if (!$result['results'][$i]) {
                 break;
