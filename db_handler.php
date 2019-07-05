f344e6e3c2202fa79fafc3e8dc197f431d9cc25a
maselka <gabdullinmr@gmail.com>
2019-07-06 01:58:00 +0300

    отладка

--- db_handler.php
+++ db_handler.php
@@ -24,7 +24,7 @@ function insertRow($db, $id, $request, $response){
     $row = [
         'id' => $id,
         'request' => $request,
-        'response' => json_encode($response),
+       // 'response' => json_encode($response),
         'date' => new DateTime()
     ];
     $db->insert($db, json_encode($row));
