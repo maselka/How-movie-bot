7d714561e8b002ceafab37b0d80633f27ca88bda
maselka <gabdullinmr@gmail.com>
2019-07-08 15:04:58 +0300

    отладка

--- db_handler.php
+++ db_handler.php
@@ -29,7 +29,7 @@ function getResponse(MysqliDb $db, $request){
     $date_request = DateTime::createFromFormat('Y-m-d', $cach_request[0]['date']);
     $date_now = new DateTime('now');
 
-    $date_diff = date_diff($date_now, $date_request);//->format('%a');
+    $date_diff = date_diff($date_now, $date_request)->format('%a');
     //$date_diff=$date_request->diff($date_now)->format("%a");
     error_log(var_export($date_request, true));
     error_log(var_export($date_now, true));
